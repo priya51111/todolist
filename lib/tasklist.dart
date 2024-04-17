ba2812aceb7913407a536b4183d59b78e6fa6376
jesi28 <jesila.arulraj@gmail.com>
2024-04-17 19:54:03 +0530

    jesi

---
+++ lib/tasklist.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class tasklist extends StatelessWidget {
+  const tasklist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue),
+    );
+  }
+}
